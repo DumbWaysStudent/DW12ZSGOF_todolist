9e2462884d5e73a2dac5a61577d3dc13e54cd8cf
Krunstudio <krunstdio@gmail.com>
2019-10-07 21:12:32 +0700

    add 2.add_todo

--- App.js
+++ App.js
@@ -1,35 +1,68 @@
 import React from 'react';
 import { View, Text, StyleSheet} from 'react-native';
+import { Container, Content, Input, Item, Button} from 'native-base';
+
 
 
 class App extends React.Component {
     
-    constructor() {
-        super();
+  constructor(props) {
+    
+    super(props)
+ 
+    this.state = {
+      todo : [
+        {id: 0, task : 'Work'},
+        {id: 1, task : 'Swim'},
+        {id: 2, task : 'Study'},
+        {id: 3, task : 'Sleep'},
+        {id: 4, task : 'Run'}
+      ],
+ 
+      Holder: ''
+ 
     }
+ 
+  }
+
+add_todo=()=>{
+
+   //Adding Items To Array.
+   this.state.todo.push({id : 5, task : this.state.Holder});
+   this.setState({Holder:''})
+}
 
     render() {
 
 
-        const todo = [
-          'work',
-          'swim',
-          'study',
-          'sleep',
-          'run'
-        ];
+        
         return (
- 
 
-          <View >
-                {todo.map((item, index) => {
+        <Container>
+        <Content>
+          <View style={styles.add_todo}>
+          <Item regular style={styles.input}>
+          <Input placeholder='New Todo' onChangeText={(text)=>{this.setState({Holder:text})}} value={this.state.Holder}/>
+          </Item>
+          <Button success onPress={()=> this.add_todo()} style={styles.button}><Text> ADD </Text></Button>
+          </View>
+          
+          
+
+          <View>
+                {this.state.todo.map((item, index) => {
+                  
                     return (
-                        <View key={index} style={styles.item}>
-                            <Text >{item}</Text>
+                        <View key={item.id} style={styles.item} >
+                            <Text style={styles.list}>{item.task}</Text> 
                         </View>
                     )
+                    
                 })} 
           </View>
+
+        </Content>
+      </Container>
         )
     }
 }
@@ -38,8 +71,34 @@ const styles = StyleSheet.create({
   item: {
     padding:10,
     marginVertical: 2,
-    borderBottomWidth: 2,
+    borderBottomWidth: 1,
+    flexDirection: "row",
   },
+  input: {
+    flex:8,
+    height: 50,
+    marginRight:15
+  },
+  button : {
+    flex:2,
+    height: 50,
+    justifyContent: "center"
+  },
+  add_todo : {
+    flexDirection: "row",
+    margin: 10,
+    paddingTop : 10,
+    paddingBottom : 30
+  },
+  list : {
+    flex: 8,
+  },
+  list : {
+    flex: 5,
+  },
+  checkbox : {
+    flex: 1,
+  }
 })
 
 
